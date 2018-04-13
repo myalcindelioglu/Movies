a84258f317126232198fb478cd9a3f53c21d7238
ttmdelioglu <yalcin.delioglu@turkcell.com.tr>
2018-04-14 00:53:43 +0300

    Movie Detail layout is added.

--- app/src/main/java/com/myd/movies/mvp/model/MovieDetails.java
+++ app/src/main/java/com/myd/movies/mvp/model/MovieDetails.java
@@ -79,7 +79,6 @@ public class MovieDetails {
         this.overview = overview;
     }
 
-
     @Override
     public String toString() {
         return "MovieDetails{" +
@@ -92,5 +91,34 @@ public class MovieDetails {
                 ", vote_count=" + vote_count +
                 ", overview='" + overview + '\'' +
                 '}';
+    }
+
+    public class Genres {
+        private int id;
+        private String name;
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "Genres{" +
+                    "id=" + id +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
     }
 }
